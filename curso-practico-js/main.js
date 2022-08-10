ac545b1a2d435a6aeac1f14abe550e25cfb11b84
Javi Llinares <javierllinaresbarral@outlook.com>
2022-08-10 14:14:56 +0200

    uAñadida funcionalidad al menu mobile

--- curso-practico-js/main.js
+++ curso-practico-js/main.js
@@ -1,7 +1,19 @@
+//Desktop
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 menuEmail.addEventListener('click', toggleDesktopMenu);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
+}
+
+//Mobile
+const mobileHamburguer = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+mobileMenu.classList.remove('activo');
+mobileHamburguer.addEventListener('click', toggleMobileMenu);
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('activo');
 }
